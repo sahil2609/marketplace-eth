3fb77d14a98375b9a5283576be0811cb97f458e5
Sahil <g.sahil@iitg.ac.in>
2022-07-07 10:52:02 +0530

    change in info

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -43,19 +43,19 @@ export default function Course({course}) {
             { courseState === "purchased" &&
               <Message type="warning">
                 Course is purchased and waiting for the activation. Process can take up to 24 hours.
-                <i className="block font-normal">In case of any questions, please contact info@eincode.com</i>
+                <i className="block font-normal">In case of any questions, please contact 2609sahil@gmail.com</i>
               </Message>
             }
             { courseState === "activated" &&
               <Message type="success">
-                Eincode wishes you happy watching of the course.
+                We wish you happy watching of the course.
               </Message>
             }
             { courseState === "deactivated" &&
               <Message type="danger">
                 Course has been deactivated, due the incorrect purchase data.
                 The functionality to watch the course has been temporaly disabled.
-                <i className="block font-normal">Please contact info@eincode.com</i>
+                <i className="block font-normal">Please contact 2609sahil@gmail.com</i>
               </Message>
             }
           </div>
